bae72faf856eb63f9a8188c35eba557eaa934b08
Ubuntu <ubuntu@ip-172-31-18-51.us-east-2.compute.internal>
2024-12-03 16:04:05 +0000

    UPDATE: the python version

--- script/init.sh
+++ script/init.sh
@@ -3,15 +3,15 @@
 # install specific python version to make sure all the work env for various server are the same.
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
-sudo apt install python3.8 -y
-sudo apt install python3.8-distutils -y
+sudo apt install python3.12 -y
+sudo apt install python3.12-distutils -y
 
 # install awscli
 sudo apt  install awscli -y
 
 # Create a virtual environment for specific python3.8 version
 sudo apt install python3-virtualenv -y
-virtualenv --python="/usr/bin/python3.8" .v_env  
+virtualenv --python="/usr/bin/python3.12" .v_env  
 source .v_env/bin/activate 
 
 # Install dependencies
